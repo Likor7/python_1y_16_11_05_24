d5c8c13ce30f1b0900d544b737086c3162611a1e
Likor7 <sov30082003@gmail.com>
2024-11-17 13:52:01 +0200

    Started new theme Jinija

--- code/app.py
+++ code/app.py
@@ -2,15 +2,29 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+subject = "Python"
+
+group_name = "1y_16_11_05_24"
+
+students = [
+    {"name": "Sophia", "gender": "female", "age": 15},
+    {"name": "Semen", "gender": "male", "age": 13},
+    {"name": "Andrii", "gender": "male", "age": 17},
+    {"name": "Vlad", "gender": "male", "age": 16},
+    {"name": "Vova", "gender": "male", "age": 15},
+    {"name": "Rostyslav", "gender": "male", "age": 14},
+]
+
 
 @app.route("/")
 def index():
-    return render_template("./login.html")
-
-
-@app.route("/login")
-def hello():
-    return "hello world"
+    context = {
+        "title": "GoIteens",
+        "subject": subject,
+        "group_name": group_name,
+        "students": students,
+    }
+    return render_template("./index.html", **context)
 
 
 if __name__ == "__main__":
