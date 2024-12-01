3a485ed6dc387bb939a80b6abf1d2f77676bf803
Likor7 <sov30082003@gmail.com>
2024-12-01 13:53:23 +0200

    Added Classwork 1.11

--- code/app.py
+++ code/app.py
@@ -56,5 +56,10 @@ def login():
     return render_template("./login.html")
 
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("./error.html", error=error)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
