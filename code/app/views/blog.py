a10254f790e8d494bcb4ffa9f352c614fbac083d
Likor7 <sov30082003@gmail.com>
2024-12-29 13:40:19 +0200

    Added classwork 32

--- code/app/views/blog.py
+++ code/app/views/blog.py
@@ -5,4 +5,13 @@ blog_blueprint = Blueprint("blog", __name__, url_prefix="/blog")
 
 @blog_blueprint.route("/")
 def index():
-    return render_template("index.html", title="Блог", current_page=request.endpoint)
+    return render_template(
+        "blog/index.html", title="Блог", current_page=request.endpoint
+    )
+
+
+@blog_blueprint.route("/post")
+def add_post():
+    return render_template(
+        "blog/add_post.html", title="Додати пост", current_page=request.endpoint
+    )
